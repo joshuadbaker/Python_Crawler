01bc718b8c9361fe9a26c49509ba8afc9a2f53bb
joshuadbaker <jbakercoder@gmail.com>
2015-11-24 13:58:22 -0500

    added index.html and routes

---
+++ config.py
@@ -0,0 +1,19 @@
+# class Config(object):
+#   DEBUG = False
+#   TESTING = False
+#   CSRF_ENABLED = True
+#   SECRET_KEY = "needs-to-be-changed"
+
+# class ProductionConfig(Config):
+#   DEBUG = False
+
+# class StageConfig(Config):
+#   DEVELOPMENT = True
+#   DEBUG = True
+
+# class DevelopmentConfig(Config):
+#   DEVELOPMENT = True
+#   DEBUG = True
+
+# class TestingConfig(Config):
+#   TESTING = True
