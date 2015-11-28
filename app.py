94fcbda9fab67a320e7b598319a7cbf09d1e840e
joshuadbaker <jbakercoder@gmail.com>
2015-11-28 18:20:40 -0500

    getting images

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
+from IPython import embed
 import os
 import requests
-from IPython import embed
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -12,13 +14,17 @@ def index():
     try:
       url = request.form['url']
       r = requests.get(url)
-      print(r.text)
+      # print(r.text)
     except:
       errors.append(
         "Unable to get URL. Please make sure it's valid and try again."
       )
-      return render_template('index.html', erros=errors)
-
+      return render_template('index.html', errors=errors)
+  if r:
+    images = BeautifulSoup(r.text, 'html.parser').find_all('img')
+    for image in images:
+      print(image["src"])
+      # print(image)  
   return render_template('index.html', errors=errors, results=results)
 
 if __name__ == '__main__':
