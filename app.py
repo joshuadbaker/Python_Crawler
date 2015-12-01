7d94c29618bb47b7921dc1a5036e8f15fc20ff21
joshuadbaker <jbakercoder@gmail.com>
2015-12-01 15:25:32 -0500

    added rq job queue but currently disabled

--- app.py
+++ app.py
@@ -24,6 +24,23 @@ q = Queue(connection=conn)
 #   crawl = Crawl(name, crawl_id, images)
 #   return crawl
 
+def crawl(url, depth):
+  depth += 1
+  try:
+    r = requests.get(url)
+  except:
+    errors.append(
+      "Unable to get URL. Please make sure it's valid and try again."
+    )
+    return {"error": errors}
+  
+  if depth <= 2:
+    print(get_images(url))
+    links = BeautifulSoup(r.text, 'html.parser').find_all('a')
+    for link in links:
+      url = link['href']
+      crawl(url, depth)
+      # print(link['href'], depth)
 def get_images(url):
   errors = []
   try:
@@ -35,27 +52,35 @@ def get_images(url):
     return {"error": errors}
 
   images = BeautifulSoup(r.text, 'html.parser').find_all('img')
+  collection = []
   for image in images:
-    print(image["src"])
+    print(image['src'])
+    # collection.append(image["src"])
+    # collection
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   results = {}
   if request.method == "POST":
     url = request.form['url']
-    
-    job = q.enqueue_call(
-      func = get_images, args=(url,), result_ttl=5000
-    )
-
-    print(job.get_id())
-    # crawl = Crawl(url, count, [])
-    
-    # links = BeautifulSoup(r.text, 'html.parser').find_all('a')
-    # for link in links:
-    #   r = requests.get(link["href"])
-    #   print(r.text)
+    depth = 1
+    crawl(url, depth)
+    # job = q.enqueue_call(
+    #   func=crawl, args=(url, depth,), result_ttl=5000
+    # )
+    # # print(job)
+    # print(job.get_id())
   return render_template('index.html', results=results)
 
+# @app.route("/results/<job_key>", methods=['GET'])
+# def get_results(job_key):
+#   job = Job.fetch(job_key, connection=conn)
+
+#   if job.is_finished:
+#     result = Result.query.filter_by(id=job.result).first()
+#     return str(result), 200
+#   else:
+#     return "Nay!", 202
 if __name__ == '__main__':
   app.run()
