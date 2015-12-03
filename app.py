b3cc9cd379c9232f0a7423beb8043d39b61f5d25
joshuadbaker <jbakercoder@gmail.com>
2015-12-03 13:53:56 -0500

    recursive crawl works and retrieves images with a set depth for crawl

--- app.py
+++ app.py
@@ -1,48 +1,67 @@
 from flask import Flask, render_template, request
+from flask import jsonify
 from bs4 import BeautifulSoup
 from IPython import embed
 from rq import Queue
 from rq.job import Job
 from worker import conn
+from urllib.parse import urlparse
 import os
 import requests
 
 app = Flask(__name__)
 q = Queue(connection=conn)
 
-# class Crawl(object):
-#   name = ""
-#   crawl_id = 0
-#   images = []
+class Crawl(object):
+  name = ""
+  crawl_id = 0
+  images = []
 
-#   def __init__(self, name, crawl_id, images):
-#     self.name = name
-#     self.crawl_id = crawl_id
-#     self.images = images
+  def __init__(self, name, crawl_id, images):
+    self.name = name
+    self.crawl_id = crawl_id
+    self.images = images
 
-# def make_crawl(name, crawl_id, images):
-#   crawl = Crawl(name, crawl_id, images)
-#   return crawl
+  def serialize(self):
+    return {
+      'name': self.name,
+      'crawl_id': self.crawl_id,
+      'images': self.images
+    }
 
-def crawl(url, depth):
-  depth += 1
-  try:
-    r = requests.get(url)
-  except:
-    errors.append(
-      "Unable to get URL. Please make sure it's valid and try again."
-    )
-    return {"error": errors}
+def create_crawl():
+  return crawl
+
+def get_urls(url, max_depth):
+  base_url = url 
+  if max_depth == 0:
+    return
+  # try:
+  r = requests.get(url)
+  # except:
+  #   errors.append(
+  #     "Unable to get URL. Please make sure it's valid and try again."
+  #   )
+  #   return {"error": errors}
   
-  if depth <= 2:
-    print(get_images(url))
-    links = BeautifulSoup(r.text, 'html.parser').find_all('a')
-    for link in links:
-      url = link['href']
-      crawl(url, depth)
-      # print(link['href'], depth)
+  links = BeautifulSoup(r.text, 'html.parser').find_all('a')
+  urls = []
+  for link in links:
+    url = base_url + link["href"]
+    get_images(url)
+    urls.append(url)
+    
+    # return urls
+    # urls = []
+    # urls.append(link['href'])
+    # # link['href']
+    # # get_images(url)
+    # print(urls)
+    get_urls(url, max_depth-1)
+
+    
+  
 def get_images(url):
-  errors = []
   try:
     r = requests.get(url)
   except:
@@ -54,33 +73,33 @@ def get_images(url):
   images = BeautifulSoup(r.text, 'html.parser').find_all('img')
   collection = []
   for image in images:
-    print(image['src'])
-    # collection.append(image["src"])
-    # collection
-
+    # print((image['src'])
+    collection.append(image["src"])
+    print(collection)
+  return collection
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   results = {}
   if request.method == "POST":
     url = request.form['url']
-    depth = 1
-    crawl(url, depth)
-    # job = q.enqueue_call(
-    #   func=crawl, args=(url, depth,), result_ttl=5000
-    # )
-    # # print(job)
-    # print(job.get_id())
+    # get_urls(url, 1)
+    job = q.enqueue_call(
+      func=get_urls, args=(url, 1), result_ttl=5000
+    )
+    print(job.get_id)
+    # create_crawl()
+    # crawl.name = url
+    # crawl.images = get_images(url)
+    # crawl.crawl_id = job.get_id()
+    # # results = jsonify(crawl_name=(crawl.name), crawl_id=(crawl.crawl_id), crawl_images=(crawl.images))
+    # print(crawl.crawl_id)
   return render_template('index.html', results=results)
 
-# @app.route("/results/<job_key>", methods=['GET'])
-# def get_results(job_key):
-#   job = Job.fetch(job_key, connection=conn)
-
-#   if job.is_finished:
-#     result = Result.query.filter_by(id=job.result).first()
-#     return str(result), 200
-#   else:
-#     return "Nay!", 202
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+  job = Job.fetch(job_key, connection=conn)
+  results = jsonify(crawl_name=(crawl.name), crawl_id=(crawl.crawl_id), crawl_images=(crawl.images))
+  return results
 if __name__ == '__main__':
   app.run()
